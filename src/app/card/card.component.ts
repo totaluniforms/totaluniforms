ae1321b0ac1a5b1b59e50ff713db54526ccd536b
Dylan Watson <lotsabackscatter@gmail.com>
2016-10-31 22:27:10 +0800

    List categories on main page

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() public category;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.category);
   }
 
 }
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,11 @@
+import { CardComponent } from './../card/card.component';
 import { Component, OnInit } from '@angular/core';
+
+interface Category {
+  name: string,
+  link: string,
+  image: string
+}
 
 @Component({
   selector: 'app-cards',
@@ -6,8 +13,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent implements OnInit {
+  private categories: Category[];
 
-  constructor() { }
+  constructor() {
+    this.categories = [
+      {
+        name: 'Workwear',
+        link: '',
+        image: 'tradies.jpg'
+      }, {
+        name: 'Work Boots',
+        link: '',
+        image: 'tradie.jpg'
+      }, {
+        name: 'PPE',
+        link: '',
+        image: 'tradies.jpg'
+      }, {
+        name: 'Office',
+        link: 'http://www.totaluniforms.com.au/office/',
+        image: 'office.jpg'
+      }, {
+        name: 'Health Beauty',
+        link: '',
+        image: 'healthcare.jpg'
+      }, {
+        name: 'Cafe & Chef',
+        link: '',
+        image: 'cafenchef.jpg'
+      }, {
+        name: "Polo's & Tee's",
+        link: '',
+        image: 'polos.jpg'
+      }, {
+        name: 'ProTEAM',
+        link: '',
+        image: 'tradies.jpg'
+      }, {
+        name: 'School',
+        link: '',
+        image: 'polos.jpg'
+      },
+    ];
+  }
 
   ngOnInit() {
   }
