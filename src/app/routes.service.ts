2d81f1510c7105773170c20897f2a71c6144d235
Dylan Watson <lotsabackscatter@gmail.com>
2016-12-20 07:50:27 +0800

    Add cases for all categories

--- src/app/routes.service.ts
+++ src/app/routes.service.ts
@@ -1,3 +1,9 @@
+import { ppe } from './data/categories/ppe.products';
+import { healthandbeauty } from './data/categories/health-and-beauty.products';
+import { cafeandchef } from './data/categories/cafe-and-chef.products';
+import { polosandtees } from './data/categories/polos-and-tees.products';
+import { proteam } from './data/categories/proteam.products';
+import { school } from './data/categories/school.products';
 import { workboots } from './data/categories/work-boots.products';
 import { office } from './data/categories/office.products';
 import { Category } from './category';
@@ -21,8 +27,20 @@ export class RoutesService {
         return workwear;
       case 'work-boots':
         return workboots;
+      case 'ppe':
+        return ppe;
       case 'office':
         return office;
+      case 'health-and-beauty':
+        return healthandbeauty;
+      case 'cafe-and-chef':
+        return cafeandchef;   
+      case 'polos-and-tees':
+        return polosandtees;
+      case 'proteam':
+        return proteam;
+      case 'school':
+        return school;                
       default:
         // TODO Go to home
         return categories;
