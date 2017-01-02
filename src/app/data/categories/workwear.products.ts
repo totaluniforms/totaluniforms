9da3b10456f314a4a20f23dfdb7a68b4829673e8
Dylan Watson <lotsabackscatter@gmail.com>
2017-01-02 23:16:40 +0800

    Add 2 Bisley Workwear products

--- src/app/data/categories/workwear.products.ts
+++ src/app/data/categories/workwear.products.ts
@@ -16,14 +16,61 @@ export var workwear: Product[] = [
     image: 'products/dnc/3209250.jpg',
     description: '190gsm cotton drill, Full open front, gusset sleeve.'
   }, {
-    code: '3',
-    name: 'Work other thing',
-    description: 'A description about the work Shirt',
-    image: 'tradies.jpg'
+    code: 'BK6709',
+    brand: 'BISLEY',
+    name: 'MENS DRILL JACKET',
+    description: `This is the drill jacket. 
+
+They’re great when you go camping or swagging as they’ve got quilt lining in the inside so they’re absolutely warm as toast when she’s about minus 2. 
+
+There are handy pockets galore in this jacket and there’s also a couple of pockets on the side to keep the mitts warm. 
+
+Another great thing about this jacket is you can whack it in the washing machine to keep it nice and clean. You beauty!
+FABRIC
+
+100% Cotton Preshrunk Drill 310gsm 100% Cotton Preshrunk Lining 100% Polyester Wadding (Quilt Filling)
+FEATURES
+
+Cotton quilted lining with insulated padding
+YKK zip front closure
+2 angled side welt pockets
+Internal zip pocket on the left side
+Internal phone pocket on the right side with touch tape
+Partial elasticated sleeve cuff`,
+    image: 'products/bisley/BK6709.jpg'
   }, {
-    code: '4',
-    name: 'Another work thing',
-    image: 'office.jpg',
-    description: 'A description about the work Shirt'
+    code: 'BP6007',
+    brand: 'Bisley',
+    name: 'ORIGINAL COTTON DRILL MENS WORK PANTS',
+    image: 'products/bisley/BP6007.jpg',
+    description: `The original cotton drill work pant – one of my favourites. 
+
+I’ve been wearing these guys for years. I love the fact that they’re comfortable, and they’re pretty stylish as well. 
+
+They have a new innovation – the old velcro-tipped phone pocket on the side. It’s got seven belt buckles here, because I like to wear a belt to keep the strides up, and they’ve got the angled pockets on the sides to keep the mitts warm in the middle of winter when you’re working out on site. 
+
+These are a cracker.
+FABRIC
+
+100% Cotton Preshrunk Drill 310gsm
+FEATURES
+
+Mobile phone pocket on right side
+7 reinforced belt loops
+Single front pleats
+Twin back hip patch pockets
+2 angled side pockets
+Coin pocket in waistband
+YKK branded zip front fly
+SIZE
+
+Regular 77-112
+Stout 87-132
+Long 74-94
+ADDITIONAL SPECS
+
+Static Control (AS.NZS 1020:1995)
+Suitable for Industrial environments and work places
+Sun Protection 50+ (NZS 4399:1996)`
   },
 ];
