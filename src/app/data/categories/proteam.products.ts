e662aa8f18f27c3aa2932df66b7a1f8c2f270f68
Dylan Watson <lotsabackscatter@gmail.com>
2017-01-03 23:55:23 +0800

    Remove bogus products from categories

--- src/app/data/categories/proteam.products.ts
+++ src/app/data/categories/proteam.products.ts
@@ -1,21 +1,5 @@
 import { Product } from './../../product';
 
 export var proteam: Product[] = [
-  {
-    code: 'workwear',
-    name: 'Boots',
-    image: 'tradies.jpg'
-  }, {
-    code: 'work-boots',
-    name: 'Another Boot',
-    image: 'tradie.jpg'
-  }, {
-    code: 'ppe',
-    name: 'More Boots',
-    image: 'tradies.jpg'
-  }, {
-    code: 'office',
-    name: 'Bigger Boots',
-    image: 'office.jpg'
-  },
+
 ];
