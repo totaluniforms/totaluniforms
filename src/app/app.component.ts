23a3e77c24589db80df1b62bbb4a6c88fe39ee1d
Dylan Watson <lotsabackscatter@gmail.com>
2017-01-03 23:51:22 +0800

    Simply thumbnail subtitle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
 
   constructor() {
     console.log('loaded!')
